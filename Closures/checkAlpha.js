afa9ba92d2d671bedce994d4419dd305ddcd042c
Nathan Rydel <nathanrydel@gmail.com>
2023-05-24 14:32:45 -0700

    updated checkAlpha

--- Closures/checkAlpha.js
+++ Closures/checkAlpha.js
@@ -1,3 +1,6 @@
 /*Declare a function 'checkAlphabeticalOrder' which takes in no arguments and returns another function. The returned function should take in a single argument (it can be a string or -1) and when invoked with -1, it should check if all passed-in strings is in alphabetical order. If so, return "Great! The strings you entered are in alphabetical order", otherwise, return an array of strings containing segments in alphabetical order.
 */
+// declare a function that takes no args
+function checkAlphabeticalOrder() {
 
+}
