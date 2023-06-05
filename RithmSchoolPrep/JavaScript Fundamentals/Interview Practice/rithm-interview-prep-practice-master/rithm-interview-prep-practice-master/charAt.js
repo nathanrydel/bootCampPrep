9b9001d2a7938586a64c50549d0c3e9950fef968
nathanrydel <nathanrydel@gmail.com>
2023-06-04 23:12:54 -0700

    charAt challenge complete

--- RithmSchoolPrep/JavaScript Fundamentals/Interview Practice/rithm-interview-prep-practice-master/rithm-interview-prep-practice-master/charAt.js
+++ RithmSchoolPrep/JavaScript Fundamentals/Interview Practice/rithm-interview-prep-practice-master/rithm-interview-prep-practice-master/charAt.js
@@ -1,4 +1,15 @@
-// add whatever parameters you deem necessary - good luck!
-function charAt(){
+// Write a function called **charAt** which accepts a string and an index (number) and returns the character at that index.
 
+// The function should return an empty string if the number is greater than the length of the string.
+
+// **Do not use the built in charAt method**
+
+function charAt(str, idx) {
+  if (idx >= str.length) {
+    return "";
+  }
+  return str[idx];
 }
+
+console.log(charAt("awesome", 2)); // 'e'
+console.log(charAt("awesome", 12)); // ''
