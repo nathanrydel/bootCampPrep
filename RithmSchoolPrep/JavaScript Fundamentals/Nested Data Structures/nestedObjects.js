c0a38ee1d94259cf85ee1b84200fa7d319ac1135
nathanrydel <nathanrydel@gmail.com>
2023-06-04 21:28:02 -0700

    Nested Objects Module complete

--- RithmSchoolPrep/JavaScript Fundamentals/Nested Data Structures/nestedObjects.js
+++ RithmSchoolPrep/JavaScript Fundamentals/Nested Data Structures/nestedObjects.js
@@ -1,20 +1,60 @@
-Exercises
-Write a function called displayCities that console.logs all the values in the citiesLivedIn array:
+let instructorData = {
+  name: "Elie",
+  additionalData: {
+    instructor: true,
+    favoriteHobbies: ["Playing Cello", "Tennis", "Coding"],
+    moreDetails: {
+      favoriteBasketballTeam: "New York Knicks",
+      numberOfSiblings: 3,
+      isYoungest: true,
+      hometown: {
+        city: "West Orange",
+        state: "NJ",
+      },
+      citiesLivedIn: ["Seattle", "Providence", "New York"],
+    },
+  },
+};
+
+// Exercises
+// Write a function called displayCities that console.logs all the values in the citiesLivedIn array:
+
+function displayCities() {
+  let citiesLivedIn = instructorData.additionalData.moreDetails.citiesLivedIn;
+  for (let i = 0; i < citiesLivedIn.length; i++) {
+    console.log(citiesLivedIn[i]);
+  }
+}
 
 displayCities();
 
 // "Seattle"
 // "Providence"
 // "New York"
-Write a function called displayHometownData that console.logs all the values inside of the hometown object
+
+// Write a function called displayHometownData that console.logs all the values inside of the hometown object
+
+function displayHometownData() {
+  let hometown = instructorData.additionalData.moreDetails.hometown;
+  for (const key in hometown) {
+    console.log(hometown[key]);
+  }
+}
 
 displayHometownData();
 
 // "West Orange"
 // "NJ"
-Let’s write a function called addDetail that accepts two parameters, a key and a value and adds the key and value to the moreDetails object and returns the moreDetails object
+
+// Let’s write a function called addDetail that accepts two parameters, a key and a value and adds the key and value to the moreDetails object and returns the moreDetails object
+
+function addDetail(key, value) {
+  instructorData.additionalData.moreDetails[key] = value;
+  console.log(instructorData.additionalData.moreDetails);
+}
 
 addDetail("isHilarious", true);
+
 /*
 {
     favoriteBasketballTeam: "New York Knicks",
@@ -45,7 +85,16 @@ addDetail("previousApartments", ["Mission", "North Beach", "Nob Hill"]);
 */
 // Finally, let’s write a function called removeDetail that removes a key in the moreDetails object and returns the moreDetails object (the new keys added above are not included in these examples).
 
-removeDetail('citiesLivedIn');
+function removeDetail(key) {
+  let moreDetails = instructorData.additionalData.moreDetails;
+  // eslint-disable-next-line no-prototype-builtins
+  if (moreDetails.hasOwnProperty(key)) {
+    delete moreDetails[key];
+    console.log(moreDetails);
+  }
+}
+
+removeDetail("citiesLivedIn");
 /*
 {
     favoriteBasketballTeam: "New York Knicks",
@@ -57,7 +106,7 @@ removeDetail('citiesLivedIn');
     }
 }
 */
-removeDetail('hometown');
+removeDetail("hometown");
 /*
 {
     favoriteBasketballTeam: "New York Knicks",
@@ -65,7 +114,7 @@ removeDetail('hometown');
     isYoungest: true
 }
 */
-removeDetail('favoriteBasketballTeam');
+removeDetail("favoriteBasketballTeam");
 /*
 {
     numberOfSiblings: 3,
