38613a176b1cbe41701c4289224ba6866edc905e
nathanrydel <nathanrydel@gmail.com>
2023-06-04 23:13:06 -0700

    append challenge complete

--- RithmSchoolPrep/JavaScript Fundamentals/Interview Practice/rithm-interview-prep-practice-master/rithm-interview-prep-practice-master/appendToString.js
+++ RithmSchoolPrep/JavaScript Fundamentals/Interview Practice/rithm-interview-prep-practice-master/rithm-interview-prep-practice-master/appendToString.js
@@ -1,4 +1,11 @@
-// add whatever parameters you deem necessary - good luck!
-function appendToString(){
+// The function should return a new string which consists of the second string appended to the first string.
 
+function appendToString(str1, str2) {
+  return str1 + str2;
 }
+
+console.log(appendToString("Hello", " World!")); // "Hello World!"
+console.log(appendToString("Foo", "bar")); // "Foobar"
+console.log(appendToString("bar", "Foo")); // "barFoo"
+console.log(appendToString("", "test")); // "test"
+console.log(appendToString("other test", "")); // "other test"
