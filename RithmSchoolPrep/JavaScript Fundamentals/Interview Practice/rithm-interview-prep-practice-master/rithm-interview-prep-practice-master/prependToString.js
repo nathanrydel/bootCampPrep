63046600d039976968efb77c993b6e9519727feb
nathanrydel <nathanrydel@gmail.com>
2023-06-04 23:12:42 -0700

    prepend challenge completed

--- RithmSchoolPrep/JavaScript Fundamentals/Interview Practice/rithm-interview-prep-practice-master/rithm-interview-prep-practice-master/prependToString.js
+++ RithmSchoolPrep/JavaScript Fundamentals/Interview Practice/rithm-interview-prep-practice-master/rithm-interview-prep-practice-master/prependToString.js
@@ -1,4 +1,13 @@
-// add whatever parameters you deem necessary - good luck!
-function prependToString(){
+// Write a function called **prependToString,** which accepts two strings.
 
+// The function should return a new string with the second string prepended to the first string.
+
+// Examples:
+
+console.log(prependToString("awesome", "very")); // 'veryawesome'
+console.log(prependToString("world", "hello ")); // 'hello world'
+console.log(prependToString("nothing", "")); // 'nothing'
+
+function prependToString(str1, str2) {
+  return str2 + str1;
 }
