2c88ccb2d4b3abf6cac63476c17b5dc5339740ad
nathanrydel <nathanrydel@gmail.com>
2023-06-04 23:22:14 -0700

    string includes challenge complete

--- RithmSchoolPrep/JavaScript Fundamentals/Interview Practice/rithm-interview-prep-practice-master/rithm-interview-prep-practice-master/stringIncludes.js
+++ RithmSchoolPrep/JavaScript Fundamentals/Interview Practice/rithm-interview-prep-practice-master/rithm-interview-prep-practice-master/stringIncludes.js
@@ -1,4 +1,20 @@
-// add whatever parameters you deem necessary - good luck!
-function stringIncludes(){
+// ### stringIncludes
 
+// Write a function called **stringIncludes,** which accepts two strings: the first string is a word and the second string is a single character.
+
+// The function should return `true` if the first string includes the character, otherwise it should return `false`.
+
+// Do not use the built in `String.includes()` function!
+
+// Examples:
+
+function stringIncludes(str, match) {
+  // iterate over str
+  for (const char of str) {
+    if (char === match) return true;
+  }
+  return false;
 }
+
+console.log(stringIncludes("awesome", "e")); // true
+console.log(stringIncludes("awesome", "z")); // false
