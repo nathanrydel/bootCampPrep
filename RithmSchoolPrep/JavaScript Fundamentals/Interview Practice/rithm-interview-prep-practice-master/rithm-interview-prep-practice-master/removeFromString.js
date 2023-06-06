80cd30fb5bb1ce863472821f7e1b7274482f947c
Nathan Rydel <nathanrydel@gmail.com>
2023-06-05 21:40:16 -0700

    Remove from String complete

--- RithmSchoolPrep/JavaScript Fundamentals/Interview Practice/rithm-interview-prep-practice-master/rithm-interview-prep-practice-master/removeFromString.js
+++ RithmSchoolPrep/JavaScript Fundamentals/Interview Practice/rithm-interview-prep-practice-master/rithm-interview-prep-practice-master/removeFromString.js
@@ -1,4 +1,19 @@
-// add whatever parameters you deem necessary - good luck!
-function removeFromString(){
+// Write a function called **removeFromString,** which accepts a string, a starting index (number) and a number of characters to remove.
 
+// The function should return a new string with the characters removed.
+
+console.log(removeFromString("Elie", 2, 2)); // 'El'
+console.log(removeFromString("Elie", 0, 1)); // 'lie'
+console.log(removeFromString("Rithm School", 0, 6)); // 'School'
+console.log(removeFromString("Rithm School", 2, 4)); // 'RiSchool'
+console.log(removeFromString("Rithm School", 6, 400)); // 'Rithm '
+
+// add whatever parameters you deem necessary - good luck!
+function removeFromString(str, idx, num) {
+  // convert to arr
+  let strToArr = str.split("");
+  // use splice to remove
+  strToArr.splice(idx, num);
+  // return joined array
+  return strToArr.join("");
 }
