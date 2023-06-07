c1a9a4cf1a4d6cccbfb0e0672b8892a9b6439b5a
Nathan Rydel <nathanrydel@gmail.com>
2023-06-06 17:21:58 -0700

    Last Index Of complete

--- RithmSchoolPrep/JavaScript Fundamentals/Interview Practice/rithm-interview-prep-practice-master/rithm-interview-prep-practice-master/lastIndexOf.js
+++ RithmSchoolPrep/JavaScript Fundamentals/Interview Practice/rithm-interview-prep-practice-master/rithm-interview-prep-practice-master/lastIndexOf.js
@@ -1,4 +1,14 @@
-// add whatever parameters you deem necessary - good luck!
-function lastIndexOf(){
+/* eslint-disable for-direction */
+console.log(lastIndexOf([1, 2, 3, 4], 2)); // 1
+console.log(lastIndexOf([1, 2, 3, 4, 2], 2)); // 4
+console.log(lastIndexOf([1, 2, 3, 4], 22)); // -1
 
+// add whatever parameters you deem necessary - good luck!
+function lastIndexOf(arr, tar) {
+  // iterate over array in descending
+  for (let i = arr.length - 1; i >= 0; i--) {
+    // check if arr at i equals tar
+    if (arr[i] === tar) return i;
+  }
+  return -1;
 }
