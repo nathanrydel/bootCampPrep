7c43fba30ddfa3c22b60dd45e466beba6d6e6f59
Nathan Rydel <nathanrydel@gmail.com>
2023-06-06 17:25:13 -0700

    Max Challenge complete

--- RithmSchoolPrep/JavaScript Fundamentals/Interview Practice/rithm-interview-prep-practice-master/rithm-interview-prep-practice-master/max.js
+++ RithmSchoolPrep/JavaScript Fundamentals/Interview Practice/rithm-interview-prep-practice-master/rithm-interview-prep-practice-master/max.js
@@ -1,4 +1,26 @@
-// add whatever parameters you deem necessary - good luck!
-function max(){
+// ### max
 
+// Write a function called **max,** which accepts an array and returns the highest value.
+
+// Do not use the built-in `Math.max()` function!
+
+// Examples:
+
+console.log(max([5, 1, 4, 7, 1, 2])); // 7
+console.log(max([3, 4, 12, 1, 8])); // 12
+console.log(max([-1, 6, 3, 2.2, -10, -4])); // 6
+
+// add whatever parameters you deem necessary - good luck!
+function max(array) {
+  // store currentHighestNum
+  let currentHighestNum = 0;
+  // iterate over arr
+  for (let num of array) {
+    // check if current element is greater than cHN
+    if (num > currentHighestNum) {
+      // if so, update cHN
+      currentHighestNum = num;
+    }
+  }
+  return currentHighestNum;
 }
