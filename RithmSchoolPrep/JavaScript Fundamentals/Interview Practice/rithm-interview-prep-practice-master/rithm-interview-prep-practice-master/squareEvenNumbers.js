b81857d36193ace5781981b9cff584ab3e85c1d2
Nathan Rydel <nathanrydel@gmail.com>
2023-06-06 17:53:57 -0700

    Square Even Numbers complete

--- RithmSchoolPrep/JavaScript Fundamentals/Interview Practice/rithm-interview-prep-practice-master/rithm-interview-prep-practice-master/squareEvenNumbers.js
+++ RithmSchoolPrep/JavaScript Fundamentals/Interview Practice/rithm-interview-prep-practice-master/rithm-interview-prep-practice-master/squareEvenNumbers.js
@@ -1,4 +1,25 @@
-// add whatever parameters you deem necessary - good luck!
-function squareEvenNumbers(){
+// ### squareEvenNumbers
 
+// Write a function called **squareEvenNumbers** which accepts an array and returns the sum of all of the even numbers in the array squared.
+
+// Examples:
+
+console.log(squareEvenNumbers([1, 2, 3, 4, 5])); // 20
+console.log(squareEvenNumbers([1, 3, 5, 7])); // 0
+console.log(squareEvenNumbers([5, 6, 7])); // 36
+
+// add whatever parameters you deem necessary - good luck!
+function squareEvenNumbers(arr) {
+  // declare total and set to 0
+  let total = 0;
+  // iterate over array
+  for (let num of arr) {
+    // check if element is even
+    if (num % 2 === 0) {
+      // if so, square it and add to total
+      total += num ** 2;
+    }
+  }
+  // return total
+  return total;
 }
