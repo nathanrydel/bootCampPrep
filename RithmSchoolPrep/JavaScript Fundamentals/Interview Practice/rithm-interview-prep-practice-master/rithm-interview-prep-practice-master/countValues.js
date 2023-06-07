c2b2288791e2a3214acc819f334dc6174a24715e
Nathan Rydel <nathanrydel@gmail.com>
2023-06-06 17:40:03 -0700

    Count Values complete

--- RithmSchoolPrep/JavaScript Fundamentals/Interview Practice/rithm-interview-prep-practice-master/rithm-interview-prep-practice-master/countValues.js
+++ RithmSchoolPrep/JavaScript Fundamentals/Interview Practice/rithm-interview-prep-practice-master/rithm-interview-prep-practice-master/countValues.js
@@ -1,4 +1,25 @@
-// add whatever parameters you deem necessary - good luck!
-function countValues(){
+// ### countValues
 
+// Write a function called **countValues** which accepts an array and a number and returns the number of times that value appears in the array.
+
+// Examples:
+
+console.log(countValues([4, 1, 4, 2, 3, 4, 4], 4)); // 4
+console.log(countValues([4, 1, 4, 2, 3, 4, 4], 100)); // 0
+console.log(countValues([], 1)); // 0
+
+// add whatever parameters you deem necessary - good luck!
+function countValues(arr, number) {
+  // initialize valCounter to 0
+  let valCounter = 0;
+  // iterate over arr
+  for (const el of arr) {
+    // check if element equals number
+    if (el === number) {
+      // if so, updated counter
+      valCounter++;
+    }
+  }
+  // return valCounter;
+  return valCounter;
 }
