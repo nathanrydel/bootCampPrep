04434c6e355b6c920fe557d6be1f51664eb264cf
Nathan Rydel <nathanrydel@gmail.com>
2023-06-06 21:13:08 -0700

    Remove Vowels

--- RithmSchoolPrep/JavaScript Fundamentals/Interview Practice/rithm-interview-prep-practice-master/rithm-interview-prep-practice-master/removeVowels.js
+++ RithmSchoolPrep/JavaScript Fundamentals/Interview Practice/rithm-interview-prep-practice-master/rithm-interview-prep-practice-master/removeVowels.js
@@ -1,4 +1,22 @@
-// add whatever parameters you deem necessary - good luck!
-function removeVowels(){
+// ### removeVowels
 
+// Write a function called **removeVowels** which will accept a string and return a new string with all the vowels removed. You should not consider "y" to be a vowel.
+
+// Examples:
+
+console.log(removeVowels("Hello!")); // "Hll!"
+console.log(removeVowels("Tomatoes")); // "Tmts"
+console.log(removeVowels("Reverse Vowels In The String")); // "Rvrs Vwls n Th Strng"
+console.log(removeVowels("aeiou")); // ""
+console.log(removeVowels("why try, shy fly?")); // "why try, shy fly?"
+
+// input: string
+// output: string
+
+// add whatever parameters you deem necessary - good luck!
+function removeVowels(string) {
+  // declare regex to look for A,E,I,O,U
+  let regex = /[aeiou]/gi;
+  // replace regex with ""
+  return string.replace(regex, "");
 }
