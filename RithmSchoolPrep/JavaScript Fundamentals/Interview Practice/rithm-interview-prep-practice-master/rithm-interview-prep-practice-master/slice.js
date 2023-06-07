c1371572339d61fd25c5d2b2afeb78f838f230cd
Nathan Rydel <nathanrydel@gmail.com>
2023-06-06 17:36:27 -0700

    slice complete

--- RithmSchoolPrep/JavaScript Fundamentals/Interview Practice/rithm-interview-prep-practice-master/rithm-interview-prep-practice-master/slice.js
+++ RithmSchoolPrep/JavaScript Fundamentals/Interview Practice/rithm-interview-prep-practice-master/rithm-interview-prep-practice-master/slice.js
@@ -1,4 +1,35 @@
-// add whatever parameters you deem necessary - good luck!
-function slice(){
+// ### slice
 
+// Write a function called **slice,** which accepts an array, and two numbers.
+
+// The function should return a new array with the elements starting at the index of the first number and going until the index of the second number.
+
+// If a third parameter is not passed to the function, it should slice until the end of the array by default.
+
+// If the third parameter is greater than the length of the array, it should slice until the end of the array.
+
+// Do not use the built in `Array.slice()` function!
+
+// Examples:
+
+console.log(slice([1, 2, 3, 4, 5], 0, 2)); // [1, 2]
+console.log(slice([1, 2, 3, 4, 5], 2, 4)); // [3, 4]
+console.log(slice([1, 2, 3, 4, 5], 2)); // [3, 4, 5]
+console.log(slice([1, 2, 3, 4, 5], 2, 10)); // [3, 4, 5]
+
+// add whatever parameters you deem necessary - good luck!
+function slice(arr, start, end) {
+  // declare newOutput array
+  let output = [];
+  //  check if end is undefined or larger than arr length
+  if (end === undefined || end > arr.length) {
+    end = arr.length;
+  }
+  // iterate over array starting at start and continuing until end
+  for (let i = start; i < end; i++) {
+    // push element at i to output
+    output.push(arr[i]);
+  }
+  // return output
+  return output;
 }
