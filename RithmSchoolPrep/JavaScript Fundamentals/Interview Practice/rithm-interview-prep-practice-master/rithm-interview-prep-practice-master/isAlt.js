82addc264e559f4058a06c8b78d73993832197a5
nathanrydel <nathanrydel@gmail.com>
2023-06-06 22:14:29 -0700

    isAlt ROE

--- RithmSchoolPrep/JavaScript Fundamentals/Interview Practice/rithm-interview-prep-practice-master/rithm-interview-prep-practice-master/isAlt.js
+++ RithmSchoolPrep/JavaScript Fundamentals/Interview Practice/rithm-interview-prep-practice-master/rithm-interview-prep-practice-master/isAlt.js
@@ -1,4 +1,12 @@
-// add whatever parameters you deem necessary - good luck!
-function isAlt(){
+// ### isAlt
 
-}
+// Create a function `isAlt` that accepts a string as an argument and validates whether the vowels (a, e, i, o, u) and consonants are in alternate order.
+
+// Examples:
+
+isAlt("amazon"); // true
+isAlt("apple"); // false
+isAlt("banana"); // true
+
+// add whatever parameters you deem necessary - good luck!
+function isAlt() {}
