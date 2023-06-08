4f036a2621686660243177801d6ab411dd3df304
Nathan Rydel <nathanrydel@gmail.com>
2023-06-07 18:16:50 -0700

    Separate Complete

---
+++ RithmSchoolPrep/JavaScript Fundamentals/Interview Practice/rithm-interview-prep-practice-master/rithm-interview-prep-practice-master/separate2.js
@@ -0,0 +1,81 @@
+// ### separate
+
+// Dogs don't get along with cats, and cats don't get along with dogs.
+
+// What they both have in common is that they don't get along with water (baths).
+
+// Given an array of 'dogs', 'cats', and 'water', write a function called **separate,** which returns a new array so that the dogs are separated from the cats by water. **Make sure that cats always come first in the array.**
+
+// You can assume that the array will always at least three elements, and that there'll always be at least one dog, one cat, and one water to work with.
+
+// **Examples:**
+
+console.log(separate(["dog", "cat", "water"])); // ['cat','water','dog']
+
+console.log(separate(["dog", "cat", "water", "cat"])); // ['cat', 'cat', 'water', 'dog'])
+
+console.log(
+  separate(["cat", "cat", "water", "dog", "water", "cat", "water", "dog"]),
+);
+
+// ['cat','cat','cat','water','water','water','dog','dog']
+
+console.log(
+  separate([
+    "cat",
+    "cat",
+    "cat",
+    "cat",
+    "cat",
+    "cat",
+    "cat",
+    "cat",
+    "cat",
+    "cat",
+    "cat",
+    "cat",
+    "cat",
+    "cat",
+    "cat",
+    "cat",
+    "cat",
+    "cat",
+    "dog",
+    "water",
+    "water",
+    "water",
+    "water",
+    "water",
+    "water",
+    "water",
+    "water",
+    "water",
+    "water",
+    "water",
+    "water",
+    "water",
+    "water",
+  ]),
+);
+
+/* ['cat','cat','cat','cat','cat',
+    'cat','cat','cat','cat','cat','cat',
+    'cat','cat','cat','cat','cat','cat','cat',
+    'water','water','water','water','water',
+    'water','water','water','water','water','water',
+    'water','water','water', 'dog']
+*/
+
+// add whatever parameters you deem necessary - good luck!
+function separate(array) {
+  // return a concated array filtered over every element
+  return (
+    []
+      // filter out cats
+      .concat(array.filter((word) => word === "cat"))
+      // filter out water
+      .concat(array.filter((word) => word === "water"))
+      // filter out dogs
+      .concat(array.filter((word) => word === "dog"))
+  );
+}
