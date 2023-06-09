cb8144b779548c6f1aafc6aa420b8d469b39bdd7
nathanrydel <nathanrydel@gmail.com>
2023-06-08 22:01:36 -0700

    Problem 1 & Problem 2

--- ChatGPT Prep/Recursion/r1.js
+++ ChatGPT Prep/Recursion/r1.js
@@ -1,8 +1,35 @@
 // Problem 1:
 // Write a recursive function to calculate the factorial of a given positive integer.
 
+function factorial(number) {
+  if (number === 1 || number === 0) {
+    return 1;
+  }
+  return number * factorial(number - 1);
+}
+
+console.log(factorial(5));
+
 // Problem 2:
 // Write a recursive function to find the nth Fibonacci number, given an input n. The Fibonacci sequence is defined as follows: the first two numbers are 0 and 1, and each subsequent number is the sum of the previous two.
+
+function fib(n, cache = {}) {
+  if (n === 0 || n === 1) {
+    return n;
+  }
+
+  if (cache[n]) {
+    return cache[n];
+  }
+
+  const result = fib(n - 1, cache) + fib(n - 2, cache);
+  cache[n] = result;
+  return result;
+}
+
+console.log(fib(8));
+console.log(fib(15));
+console.log(fib(50));
 
 // Problem 3:
 // Write a recursive function to determine if a given string is a palindrome. A palindrome is a word, phrase, number, or other sequence of characters that reads the same forward and backward, ignoring spaces, punctuation, and capitalization.
