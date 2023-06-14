004b40ac8713f4044594228ce11fec8bebf037df
Nathan Rydel <nathanrydel@gmail.com>
2023-06-13 22:20:55 -0700

    second attempt ROEs

---
+++ RithmSchoolPrep/JavaScript Fundamentals/Interview Practice 2/rithm-interview-prep-practice-part-2-main/rithm-interview-prep-practice-part-2-main/findFirstMove2.js
@@ -0,0 +1,17 @@
+// ### findFirstMove
+
+// Write a function called `findFirstMove`. This function accepts an array of arrays of moves and a move. It should return an array with the index of the first move in the array of arrays of moves. If the move is not present in the moves array, the function should return -1.
+
+// Examples:
+
+let moves = [
+  ["a", "b", "c"],
+  ["d", "a", "f"],
+  ["g", "h", "h"],
+];
+
+console.log(findFirstMove(moves, "a")); // [0, 0]
+console.log(findFirstMove(moves, "h")); // [2, 1]
+console.log(findFirstMove(moves, "z")); // -1
+
+function findFirstMove() {}
---
+++ RithmSchoolPrep/JavaScript Fundamentals/Interview Practice 2/rithm-interview-prep-practice-part-2-main/rithm-interview-prep-practice-part-2-main/findHighestPriorityTodo2.js
@@ -0,0 +1,41 @@
+// ### findHighestPriorityTodo
+
+// Write a function called `findHighestPriorityTodo` which accepts an array of objects, each of which has a 'priority' property as well as a 'task' property. The function should return an array where the first index is the name of the highest priority task and the second index is the priority rating of that same task.
+
+// Examples:
+
+let todos1 = [
+  {
+    task: "Eat",
+    priority: 18,
+  },
+  {
+    task: "Sleep",
+    priority: 22,
+  },
+  {
+    task: "Solve problems",
+    priority: 17,
+  },
+];
+
+console.log(findHighestPriorityTodo(todos1)); // ["Sleep", 22]
+
+let todos2 = [
+  {
+    task: "Task 1",
+    priority: 1,
+  },
+  {
+    task: "Task 2",
+    priority: 2,
+  },
+  {
+    task: "Task 3",
+    priority: 3,
+  },
+];
+
+console.log(findHighestPriorityTodo(todos2)); // ["Task 3", 3]
+
+function findHighestPriorityTodo() {}
