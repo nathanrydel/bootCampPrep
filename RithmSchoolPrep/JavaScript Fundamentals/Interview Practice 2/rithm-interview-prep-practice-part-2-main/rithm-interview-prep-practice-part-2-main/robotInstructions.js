fb82f03014d70bac8247258d7a8349ac90f2b812
Nathan Rydel <43457298+nathanrydel@users.noreply.github.com>
2023-06-14 15:10:45 -0700

    Robot Instructions complete

--- RithmSchoolPrep/JavaScript Fundamentals/Interview Practice 2/rithm-interview-prep-practice-part-2-main/rithm-interview-prep-practice-part-2-main/robotInstructions.js
+++ RithmSchoolPrep/JavaScript Fundamentals/Interview Practice 2/rithm-interview-prep-practice-part-2-main/rithm-interview-prep-practice-part-2-main/robotInstructions.js
@@ -4,12 +4,30 @@
 
 // Examples:
 
-robotInstructions(["U", "D", "L", "R"]);
+console.log(robotInstructions(["U", "D", "L", "R"]));
 // { "U": 1, "D": 1, "L": 1, "R": 1 })
 
 robotInstructions(["U", "D", "L", "R", "U", "D", "L", "R", "U", "D", "L", "R"]);
 // { "U": 3, "D": 3, "L": 3, "R": 3 })
 
-function robotInstructions() {
-  // add whatever parameters you deem necessary - good luck!
+// input: array
+// output: object with keys as moves of array and value as number of times in array
+
+function robotInstructions(array) {
+  // create new output obj
+  const movesObj = {}
+
+  // iterate over the array
+  for (let move of array) {
+    // check if the move already in movesObj
+    if (movesObj[move]){
+      // if so, increment value
+      movesObj[move]++;
+    } else {
+      // update the movesObj at move with value 1
+      movesObj[move] = 1;
+    }
+  }
+    // return movesObj;
+  return movesObj;
 }
