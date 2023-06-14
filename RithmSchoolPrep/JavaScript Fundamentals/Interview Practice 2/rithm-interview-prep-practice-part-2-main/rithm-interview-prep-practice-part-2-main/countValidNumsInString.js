e9d5fd1b256c5224a186949b4fad02d831b58305
Nathan Rydel <nathanrydel@gmail.com>
2023-06-13 17:16:14 -0700

    Count Valid Numbers in String complete

--- RithmSchoolPrep/JavaScript Fundamentals/Interview Practice 2/rithm-interview-prep-practice-part-2-main/rithm-interview-prep-practice-part-2-main/countValidNumsInString.js
+++ RithmSchoolPrep/JavaScript Fundamentals/Interview Practice 2/rithm-interview-prep-practice-part-2-main/rithm-interview-prep-practice-part-2-main/countValidNumsInString.js
@@ -4,14 +4,22 @@
 
 // Examples:
 
-countValidNumsInString(""); // 0
-countValidNumsInString("1"); // 1
-countValidNumsInString("12"); // 2
-countValidNumsInString("12abc3"); // 3
-countValidNumsInString("1s2d3dsadas4"); // 4
-countValidNumsInString("512,3,4!?!"); // 5
-countValidNumsInString("123456"); // 6
+console.log(countValidNumsInString("")); // 0
+console.log(countValidNumsInString("1")); // 1
+console.log(countValidNumsInString("12")); // 2
+console.log(countValidNumsInString("12abc3")); // 3
+console.log(countValidNumsInString("1s2d3dsadas4")); // 4
+console.log(countValidNumsInString("512,3,4!?!")); // 5
+console.log(countValidNumsInString("123456")); // 6
 
-function countValidNumsInString() {
-  // add whatever parameters you deem necessary - good luck!
+// input: string
+// output: count of numbers in the string
+
+function countValidNumsInString(str) {
+  // create RegEx to check for all numbers
+  const regExp = /[0-9]/g;
+  // use match to check for matches with RegEx
+  const matches = str.match(regExp);
+  // return matches length if not null OR 0
+  return matches !== null ? matches.length : 0;
 }
