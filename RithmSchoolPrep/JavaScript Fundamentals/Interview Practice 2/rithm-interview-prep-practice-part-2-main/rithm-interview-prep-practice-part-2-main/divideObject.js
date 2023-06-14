43a9e7d6a38c6af27d561c64015ee9b309d08195
Nathan Rydel <nathanrydel@gmail.com>
2023-06-13 21:28:05 -0700

    Divide Object Complete

--- RithmSchoolPrep/JavaScript Fundamentals/Interview Practice 2/rithm-interview-prep-practice-part-2-main/rithm-interview-prep-practice-part-2-main/divideObject.js
+++ RithmSchoolPrep/JavaScript Fundamentals/Interview Practice 2/rithm-interview-prep-practice-part-2-main/rithm-interview-prep-practice-part-2-main/divideObject.js
@@ -4,16 +4,35 @@
 
 // Examples:
 
-divideObject({
-  first: "hi",
-  second: "hello",
-  third: "hey",
-  fourth: 2,
-  fifth: "fun",
-  sixth: 10,
-});
+console.log(
+  divideObject({
+    first: "hi",
+    second: "hello",
+    third: "hey",
+    fourth: 2,
+    fifth: "fun",
+    sixth: 10,
+  }),
+);
 // [ [12], [13] ]
 
-function divideObject() {
-  // add whatever parameters you deem necessary - good luck!
+function divideObject(obj) {
+  // declare numSum
+  let numSum = 0;
+  // declare stringSum
+  let stringSum = 0;
+  // iterate over the keys in obj
+  for (let key in obj) {
+    // check if typeof value is a string
+    if (typeof obj[key] === "string") {
+      // if yes, add the string length to stringSum
+      stringSum += obj[key].length;
+    } else {
+      // else add to numSum
+      numSum += obj[key];
+    }
+  }
+
+  // return [ [numSum], [stringSum]];
+  return [[numSum], [stringSum]];
 }
