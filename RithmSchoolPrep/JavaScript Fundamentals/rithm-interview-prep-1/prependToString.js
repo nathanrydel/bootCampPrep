dbf9dbcc84839c87475b609718f6e20e3cf875dc
Nathan Rydel <nathanrydel@gmail.com>
2023-08-16 16:16:57 -0700

    PTS complete

--- RithmSchoolPrep/JavaScript Fundamentals/rithm-interview-prep-1/prependToString.js
+++ RithmSchoolPrep/JavaScript Fundamentals/rithm-interview-prep-1/prependToString.js
@@ -1,4 +1,8 @@
 // add whatever parameters you deem necessary - good luck!
-function prependToString(){
-
+function prependToString(str1, str2) {
+  return str2 + str1;
 }
+
+console.log(prependToString("awesome", "very")); // 'veryawesome'
+console.log(prependToString("world", "hello ")); // 'hello world'
+console.log(prependToString("nothing", "")); // 'nothing'
