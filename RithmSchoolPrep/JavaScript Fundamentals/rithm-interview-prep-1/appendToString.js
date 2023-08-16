7e6ec3db7fea94eb94a317ef4003ef2318f459a6
Nathan Rydel <nathanrydel@gmail.com>
2023-08-16 16:11:34 -0700

    ATS complete

--- RithmSchoolPrep/JavaScript Fundamentals/rithm-interview-prep-1/appendToString.js
+++ RithmSchoolPrep/JavaScript Fundamentals/rithm-interview-prep-1/appendToString.js
@@ -1,4 +1,10 @@
 // add whatever parameters you deem necessary - good luck!
-function appendToString(){
-
+function appendToString(str1, str2) {
+  return str1 + str2;
 }
+
+console.log(appendToString("Hello", " World!")); // "Hello World!"
+console.log(appendToString("Foo", "bar")); // "Foobar"
+console.log(appendToString("bar", "Foo")); // "barFoo"
+console.log(appendToString("", "test")); // "test"
+console.log(appendToString("other test", "")); // "other test"
